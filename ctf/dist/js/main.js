3ef93840bdb69b16093bb3a9d954d0a4a8bab38b
FlyMaple <rottensociety1987@gmail.com>
2017-02-23 23:54:20 +0800

    Update main.js

--- ctf/dist/js/main.js
+++ ctf/dist/js/main.js
@@ -1,85 +1,79 @@
-; (function (jQuery, window) {
-    jQuery(function () {
-        window.sr = ScrollReveal();
+jQuery(function () {
+    window.sr = ScrollReveal();
 
-        jQuery('body').scrollspy({ target: '.nav-nav' });
+    jQuery('body').scrollspy({ target: '.nav-nav' });
 
-        jQuery('a.page-scroll').bind('click', function (event) {
-            var $anchor = jQuery(this);
-            jQuery('html, body').stop().animate({
-                scrollTop: jQuery($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
+    jQuery('a.page-scroll').bind('click', function (event) {
+        var $anchor = jQuery(this);
+        jQuery('html, body').stop().animate({
+            scrollTop: jQuery($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 
-        jQuery('.nav-share').on('click', function (event) {
-            event.preventDefault();
+    jQuery('.nav-share').on('click', function (event) {
+        event.preventDefault();
 
-            jQuery(this).toggleClass('active');
-            jQuery('ul', this).toggleClass('active');
-        });
-        
-        // sr.reveal('#gallery .item', { duration: 3000 }, 50);
+        jQuery(this).toggleClass('active');
+        jQuery('ul', this).toggleClass('active');
+    });
 
-        jQuery('#submit').on('click', function () {
-            if (Captcha.CheckCaptcha()) {
-                alert('功能未開放!');
-            }
-        });
+    // sr.reveal('#gallery .item', { duration: 3000 }, 50);
 
-        jQuery('#CaptchaImageCode').on('click', Captcha.CreateCaptcha);
+    jQuery('#submit').on('click', function () {
+        if (Captcha.CheckCaptcha()) {
+            alert('功能未開放!');
+        }
+    });
 
-        
-        
-        document.addEventListener('load', function () {
-            var qsRegex;
-            var $grid = $('.grid').isotope({
-                itemSelector: '.grid-item',
-                filter: function() {
-                  return qsRegex ? $(this).text().match( qsRegex ) : true;
-                }	
-            });
-        });
-        // $grid.isotope('layout');
-        
-        
+    jQuery('#CaptchaImageCode').on('click', Captcha.CreateCaptcha);
 
-        // $grid.isotope({ filter: '.design' });
+    var qsRegex;
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        filter: function () {
+            return qsRegex ? $(this).text().match(qsRegex) : true;
+        }
+    });
+    // $grid.isotope('layout');
 
-        jQuery('#gallery .filters button').on('click', function () {
-            var $button = jQuery(this);
-            var filter = $button.data('filter');
 
-            if (filter === 'ALL') {
-                $grid.isotope({ filter: '' });
-            } else if (filter === 'WEBSITE') {
-                $grid.isotope({ filter: '.website' });
-            } else if (filter === 'MOBILE') {
-                $grid.isotope({ filter: '.mobile' });
-            } else if (filter === 'DESIGN') {
-                $grid.isotope({ filter: '.design' });
-            }
 
-            jQuery('#gallery .filters button').toggleClass('active', false);
-            $button.toggleClass('active', true);
-        })
+    // $grid.isotope({ filter: '.design' });
 
-        var height = jQuery(window).height();
+    jQuery('#gallery .filters button').on('click', function () {
+        var $button = jQuery(this);
+        var filter = $button.data('filter');
 
-        jQuery('#banner').css({ height: height });
-
-        if (height < 1024) {
-            jQuery('#banner').css({ height: height });
+        if (filter === 'ALL') {
+            $grid.isotope({ filter: '' });
+        } else if (filter === 'WEBSITE') {
+            $grid.isotope({ filter: '.website' });
+        } else if (filter === 'MOBILE') {
+            $grid.isotope({ filter: '.mobile' });
+        } else if (filter === 'DESIGN') {
+            $grid.isotope({ filter: '.design' });
         }
 
-        jQuery('.mMenu').on('click', function () {
-            jQuery('#nav .nav-nav').toggleClass('show');
-        });
-
-        jQuery('#main').on('click', function () {
-            jQuery('#nav .nav-nav').toggleClass('show', false);
-            jQuery('#nav .nav-share').toggleClass('active', false);
-            jQuery('#nav .nav-share ul').toggleClass('active', false);
-        });
+        jQuery('#gallery .filters button').toggleClass('active', false);
+        $button.toggleClass('active', true);
     })
-})($, window);
+
+    var height = jQuery(window).height();
+
+    jQuery('#banner').css({ height: height });
+
+    if (height < 1024) {
+        jQuery('#banner').css({ height: height });
+    }
+
+    jQuery('.mMenu').on('click', function () {
+        jQuery('#nav .nav-nav').toggleClass('show');
+    });
+
+    jQuery('#main').on('click', function () {
+        jQuery('#nav .nav-nav').toggleClass('show', false);
+        jQuery('#nav .nav-share').toggleClass('active', false);
+        jQuery('#nav .nav-share ul').toggleClass('active', false);
+    });
+});
