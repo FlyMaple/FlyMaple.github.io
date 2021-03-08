88bcf4293cc2952ef94b27f2c90eb7a0b5cf7f83
Skye.Wu <Skye.Wu@zyxel.com.tw>
2021-03-08 16:27:36 +0800

    update close icon

--- public/auto-sdd9.js
+++ public/auto-sdd9.js
@@ -1,178 +1,189 @@
 (function () {
-  const proxy = {
-    'gamma.nebula.zyxel.com': 'gammaccapi.nebula.zyxel.com',
-    'omega.nebula.zyxel.com': 'omegaccapi.nebula.zyxel.com',
-  };
-
-  function getOrgId() {
-    const match = location.hash.match(/#\/(.+?)(?=\/)/);
-
-    if (match) {
-      return match[1];
-    }
-
-    return null;
-  }
-
-  function getToken() {
-    const token = sessionStorage.getItem('nebula://session/x-auth-token');
-
-    return token ? token.replace(/"/g, '') : null;
-  }
-
-  function getProxy(hostname) {
-    if (hostname.includes('aio')) {
-      return `${hostname}:4430`;
-    }
-    return proxy[hostname];
-  }
-
-  function getAdmins(uri, apiUri, orgId, token) {
-    return fetch(`${apiUri}/nebula/v3/organization/${orgId}/administrator`, {
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'no-cache',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'x-auth-renew': 'true',
-        'x-auth-token': token,
-        'x-refer-page': `/${orgId}/organization-wide/configure/administrators`,
-      },
-      referrer: `${uri}/cc/ui/index.html`,
-      referrerPolicy: 'no-referrer-when-downgrade',
-      body: null,
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include',
-    }).then((res) => res.json());
-  }
-
-  function addAdmins(uri, apiUri, orgId, token, admins) {
-    return fetch(`${apiUri}/nebula/v3/organization/${orgId}/administrator`, {
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'no-cache',
-        'content-type': 'application/json',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'x-auth-renew': 'true',
-        'x-auth-token': token,
-        'x-refer-page': `/${orgId}/organization-wide/configure/administrators`,
-      },
-      referrer: `${uri}/cc/ui/index.html`,
-      referrerPolicy: 'no-referrer-when-downgrade',
-      body: JSON.stringify(admins),
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'include',
-    }).then((res) => res.json());
-  }
-
-  function createSDD9() {
-    return {
-      org_priv: {
-        access: 'FULL',
-      },
-      site_priv: {},
-      sitetag_priv: {},
-      admin_name: 'SDD9.RD(Tool)',
-      activated: true,
-      admin_email: 'sdd9.rd@gmail.com',
+    const proxy = {
+        'gamma.nebula.zyxel.com': 'gammaccapi.nebula.zyxel.com',
+        'omega.nebula.zyxel.com': 'omegaccapi.nebula.zyxel.com',
     };
-  }
 
-  function createVNRD() {
-    return {
-      org_priv: {
-        access: 'FULL',
-      },
-      site_priv: {},
-      sitetag_priv: {},
-      admin_name: 'VNRD(Tool)',
-      activated: true,
-      admin_email: 'vn.zyxel@gmail.com',
-    };
-  }
+    function getOrgId() {
+        const match = location.hash.match(/#\/(.+?)(?=\/)/);
 
-  async function addSDD9() {
-    const { hostname, protocol } = location;
-    const uri = `${protocol}//${hostname}`;
-    const proxyUri = getProxy(hostname);
-    const apiUri = `${protocol}//${getProxy(hostname)}`;
+        if (match) {
+            return match[1];
+        }
 
-    const orgId = getOrgId();
-    const token = getToken();
-
-    if (!token) {
-      alert("Can't find token.");
-      return;
+        return null;
     }
 
-    if (!orgId || !token) {
-      alert("Can't find org id.");
-      return;
+    function getToken() {
+        const token = sessionStorage.getItem('nebula://session/x-auth-token');
+
+        return token ? token.replace(/"/g, '') : null;
     }
 
-    if (!proxyUri) {
-      alert("Can't find website proxy.");
-      return;
+    function getProxy(hostname) {
+        if (hostname.includes('aio')) {
+            return `${hostname}:4430`;
+        }
+        return proxy[hostname];
     }
 
-    const admins = (await getAdmins(uri, apiUri, orgId, token)).body;
-
-    if (Array.isArray(admins)) {
-      const sdd9 = admins.find((admin) => {
-        return admin.admin_email === 'sdd9.rd@gmail.com';
-      });
-      const vnrd = admins.find((admin) => {
-        return admin.admin_email === 'vn.zyxel@gmail.com';
-      });
-
-      if (sdd9 && vnrd) {
-        alert('Already exists.');
-        return;
-      }
-
-      const adminPayload = [];
-      if (!sdd9) {
-        adminPayload.push(createSDD9());
-      }
-      if (!vnrd) {
-        adminPayload.push(createVNRD());
-      }
-      const response = await addAdmins(uri, apiUri, orgId, token, adminPayload);
-      if (response.status === 200) {
-        alert('Successful.');
-      } else {
-        alert('Failure.');
-      }
+    function getAdmins(uri, apiUri, orgId, token) {
+        return fetch(`${apiUri}/nebula/v3/organization/${orgId}/administrator`, {
+            headers: {
+                accept: 'application/json, text/plain, */*',
+                'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'no-cache',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'x-auth-renew': 'true',
+                'x-auth-token': token,
+                'x-refer-page': `/${orgId}/organization-wide/configure/administrators`,
+            },
+            referrer: `${uri}/cc/ui/index.html`,
+            referrerPolicy: 'no-referrer-when-downgrade',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+        }).then((res) => res.json());
     }
 
-    return Promise.resolve();
-  }
-
-  function createButton() {
-    const _d = document.querySelector('.skbutton');
-    if (_d) {
-      return;
+    function addAdmins(uri, apiUri, orgId, token, admins) {
+        return fetch(`${apiUri}/nebula/v3/organization/${orgId}/administrator`, {
+            headers: {
+                accept: 'application/json, text/plain, */*',
+                'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'no-cache',
+                'content-type': 'application/json',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'x-auth-renew': 'true',
+                'x-auth-token': token,
+                'x-refer-page': `/${orgId}/organization-wide/configure/administrators`,
+            },
+            referrer: `${uri}/cc/ui/index.html`,
+            referrerPolicy: 'no-referrer-when-downgrade',
+            body: JSON.stringify(admins),
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+        }).then((res) => res.json());
     }
 
-    const div = document.createElement('div');
-    div.classList.add('skbutton');
-    const img = document.createElement('img');
-    img.src =
-      'https://stickershop.line-scdn.net/stickershop/v1/product/10629/LINEStorePC/main.png;compress=true';
-    const span = document.createElement('span');
-    span.innerText = 'wait';
-    div.appendChild(img);
-    div.appendChild(span);
-    const style = document.createElement('style');
-    style.setAttribute('rel', 'stylesheet');
-    style.innerText = `
+    function createSDD9() {
+        return {
+            org_priv: {
+                access: 'FULL',
+            },
+            site_priv: {},
+            sitetag_priv: {},
+            admin_name: 'SDD9.RD(Tool)',
+            activated: true,
+            admin_email: 'sdd9.rd@gmail.com',
+        };
+    }
+
+    function createVNRD() {
+        return {
+            org_priv: {
+                access: 'FULL',
+            },
+            site_priv: {},
+            sitetag_priv: {},
+            admin_name: 'VNRD(Tool)',
+            activated: true,
+            admin_email: 'vn.zyxel@gmail.com',
+        };
+    }
+
+    async function addSDD9() {
+        const { hostname, protocol } = location;
+        const uri = `${protocol}//${hostname}`;
+        const proxyUri = getProxy(hostname);
+        const apiUri = `${protocol}//${getProxy(hostname)}`;
+
+        const orgId = getOrgId();
+        const token = getToken();
+
+        if (!token) {
+            alert("Can't find token.");
+            return;
+        }
+
+        if (!orgId || !token) {
+            alert("Can't find org id.");
+            return;
+        }
+
+        if (!proxyUri) {
+            alert("Can't find website proxy.");
+            return;
+        }
+
+        const admins = (await getAdmins(uri, apiUri, orgId, token)).body;
+
+        if (Array.isArray(admins)) {
+            const sdd9 = admins.find((admin) => {
+                return admin.admin_email === 'sdd9.rd@gmail.com';
+            });
+            const vnrd = admins.find((admin) => {
+                return admin.admin_email === 'vn.zyxel@gmail.com';
+            });
+
+            if (sdd9 && vnrd) {
+                alert('Already exists.');
+                return;
+            }
+
+            const adminPayload = [];
+            if (!sdd9) {
+                adminPayload.push(createSDD9());
+            }
+            if (!vnrd) {
+                adminPayload.push(createVNRD());
+            }
+            const response = await addAdmins(uri, apiUri, orgId, token, adminPayload);
+            if (response.status === 200) {
+                alert('Successful.');
+            } else {
+                alert('Failure.');
+            }
+        }
+
+        return Promise.resolve();
+    }
+
+    function createButton() {
+        const _d = document.querySelector('.skbutton');
+        if (_d) {
+            return;
+        }
+
+        const div = document.createElement('div');
+        div.classList.add('skbutton');
+        const img = document.createElement('img');
+        img.src = 'https://stickershop.line-scdn.net/stickershop/v1/product/10629/LINEStorePC/main.png;compress=true';
+        const span = document.createElement('span');
+        span.innerText = 'wait';
+        const close = document.createElement('div');
+        close.classList.add('close');
+        close.innerText = '✖';
+        close.addEventListener(
+            'click',
+            function (e) {
+                e.stopPropagation();
+                div.classList.add('hide');
+            },
+            false
+        );
+        div.appendChild(img);
+        div.appendChild(span);
+        div.appendChild(close);
+        const style = document.createElement('style');
+        style.setAttribute('rel', 'stylesheet');
+        style.innerText = `
           .skbutton {
                   position: fixed;
                   right: 35px;
@@ -191,6 +202,9 @@
                   background-color: #fff;
                   z-index: 1000;
           }
+          .skbutton.hide{
+            display: none;
+          }
           .skbutton > img {
               width: 100%;
               height: auto;
@@ -199,19 +213,26 @@
               display: none;
               color: #3c9f00;
           }
+          .skbutton .close {
+            position: absolute;
+            top: -8px;
+            right: -12px;
+            padding: 4px;
+            cursor: pointer;
+          }
       `;
-    div.addEventListener('click', async function () {
-      img.style.display = 'none';
-      span.style.display = 'inline-block';
+        div.addEventListener('click', async function () {
+            img.style.display = 'none';
+            span.style.display = 'inline-block';
 
-      await addSDD9();
+            await addSDD9();
 
-      img.style.display = 'inline-block';
-      span.style.display = 'none';
-    });
-    document.head.appendChild(style);
-    document.body.appendChild(div);
-  }
+            img.style.display = 'inline-block';
+            span.style.display = 'none';
+        });
+        document.head.appendChild(style);
+        document.body.appendChild(div);
+    }
 
-  createButton();
+    createButton();
 })();
