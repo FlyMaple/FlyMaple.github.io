2cb953202fb74f0aca932ad653633cf90644919d
Skye.Wu <Skye.Wu@zyxel.com.tw>
2021-07-06 11:23:30 +0800

    upload git MRs append commit behind tip

---
+++ public/git-commit-behind.js
@@ -0,0 +1,130 @@
+(async () => {
+    const styleId = 'sk-commit-behind';
+
+    const style = `
+        .sk-commit-behind {
+            margin-left: 7px;
+            opacity: .9;
+        }
+        
+        .sk-commit-behind.sk-commit-behind--rebase {
+            margin-left: 7px;
+            opacity: 1;
+            color: red;
+            font-size: 1.2em;
+        }
+    `;
+
+    function appendStyle(context) {
+        const style = document.createElement('style');
+        style.id = styleId;
+        style.textContent = context;
+        document.head.appendChild(style);
+    }
+
+    function injectStyle() {
+        if (document.querySelector(`#${styleId}`) == null) {
+            appendStyle(style);
+        }
+    }
+
+    function parseCommitBehindNumber(target) {
+        let num = -1;
+
+        const pattern = /(\d+)\scommits*\sbehind/;
+
+        const matcher = target.innerText.match(pattern);
+
+        if (matcher != null) {
+            num = parseInt(matcher[1], 10);
+        }
+
+        return num;
+    }
+
+    function iteratorMrs() {
+        let index = 0;
+        const mrs = Array.from(document.querySelectorAll('div.merge-request-title.title > span > a'));
+
+        return {
+            [Symbol.asyncIterator]: () => {
+                return {
+                    next: async () => {
+                        const mrPattern = /\/(\d+)$/;
+                        const mr = mrs[index];
+                        const mrHref = mr.href.match(mrPattern);
+
+                        if (mrHref != null) {
+                            const content = await fetch(
+                                `https://gitlab.dc.zyxel.com.tw/nebula-fe/nfe.gui2/merge_requests/${mrHref[1]}`
+                            )
+                                .then((res) => res.text())
+                                .catch((e) => '');
+                            const done = index === mrs.length - 1 ? true : false;
+
+                            index += 1;
+
+                            return {
+                                value: {
+                                    mr,
+                                    content,
+                                },
+                                done,
+                            };
+                        }
+
+                        return {
+                            done: true,
+                        };
+                    },
+                };
+            },
+        };
+    }
+
+    async function core() {
+        const parser = new DOMParser();
+
+        const iterators = iteratorMrs();
+
+        for await (const { mr, content } of iterators) {
+            try {
+                const document = parser.parseFromString(content, 'text/html');
+
+                const target = document.querySelector('.label-branch:nth-child(4)~span');
+
+                const behindNumber = parseCommitBehindNumber(target);
+
+                if (target) {
+                    const commitBehind = document.createElement('span');
+
+                    commitBehind.innerText = target.innerText;
+                    commitBehind.classList.add('sk-commit-behind');
+
+                    if (behindNumber >= 20) {
+                        commitBehind.classList.add('sk-commit-behind--rebase');
+                    }
+
+                    const nextElement = mr.nextElementSibling;
+                    if (nextElement) {
+                        nextElement.remove();
+                    }
+                    mr.after(commitBehind);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+
+    async function main() {
+        injectStyle();
+        core();
+    }
+
+    try {
+        main();
+    } catch (e) {
+        console.log('[SkyeTools] Git MR commit behind detect failure.');
+    }
+})();
